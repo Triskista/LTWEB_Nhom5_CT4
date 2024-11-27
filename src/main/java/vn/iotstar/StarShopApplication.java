f25325cfee3d3a6237da3cd95c387a7a7f322428
LuongSyNam <luongsynam1004@gmail.com>
2024-11-27 15:21:52 +0700

    jhkjhkj

--- src/main/java/vn/iotstar/StarShopApplication.java
+++ src/main/java/vn/iotstar/StarShopApplication.java
@@ -1,18 +1,7 @@
 package vn.iotstar;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import vn.iotstar.entity.Category;
-import vn.iotstar.entity.Product;
-import vn.iotstar.entity.Role;
-import vn.iotstar.entity.User;
-import vn.iotstar.repository.CategoryRepository;
-import vn.iotstar.repository.ProductRepository;
-import vn.iotstar.repository.RoleRepository;
-import vn.iotstar.repository.UserRepository;
 
 @SpringBootApplication
 public class StarShopApplication {
