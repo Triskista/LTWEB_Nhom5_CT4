d7e987cafec420bfb54e16c76a5fe5a12071e51c
LuongSyNam <luongsynam1004@gmail.com>
2024-12-04 11:08:16 +0700

    push

--- src/main/java/vn/iotstar/repository/CategoryRepository.java
+++ src/main/java/vn/iotstar/repository/CategoryRepository.java
@@ -3,6 +3,7 @@ package vn.iotstar.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,7 @@ import vn.iotstar.entity.Category;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 	Optional<Category> findByCategoryName(String categoryName);
-	List<Category> findByCategoryNameContaining(String keyword, Pageable pageable);
+	Page<Category> findByCategoryNameContaining(String keyword, Pageable pageable);
 	List<Category> findAllByCategoryName(String categoryName);
+	Optional<Category> findByCategoryId(Integer categoryId);
 }
