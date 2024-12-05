ad908b07e6d2a95131fee9612a004cc3a5fe67ee
LuongSyNam <luongsynam1004@gmail.com>
2024-12-05 17:08:18 +0700

    admin form

--- src/main/java/vn/iotstar/controller/AdminController.java
+++ src/main/java/vn/iotstar/controller/AdminController.java
@@ -1,5 +1,13 @@
 package vn.iotstar.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AdminController {
 
+	@RequestMapping("/admin")
+	public String admin() {
+		return "admin/index";
+	}
 }
